aa34923ae1e31a928ee57fb1a93bd37cd55988f9
lalit kumar <kumar932687@gmail.com>
2022-12-13 00:11:44 +0530

    tailwind css remove

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Layout from '../Components/Layout'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import 'owl.carousel/dist/assets/owl.carousel.css';  
-import 'owl.carousel/dist/assets/owl.theme.default.css'; 
-import 'tailwindcss/tailwind.css'
+
+
 
 function MyApp({ Component, pageProps }) {
  
